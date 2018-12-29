234a59532482a9de6a6e550136ffbfcbb6747fb2
eddy <cjj1992815@gmail.com>
2018-12-29 23:20:14 +1100

    add lifePhase function

--- codeChallenges/main.js
+++ codeChallenges/main.js
@@ -17,3 +17,23 @@ const agreeOrDisagree = (string1, string2) => {
         return 'You disagree!';*/
 };
 console.log(agreeOrDisagree());
+
+
+//Write a function, lifePhase(), that takes in a person's age, as a number, and returns which phase of life they are in.
+// Here are the classifications:
+// 0-3 should return 'baby'
+// 4-12 should return 'child'
+// 13-19 should return 'teen'
+// 20-64 should return 'adult'
+// 65-140 should return 'senior citizen'
+// If the number is less than 0 or greater than 140, the program should return 'This is not a valid age'
+const lifePhase = age => {
+    // ternary operator of if...else if ..else statement
+    return (age >= 0 && age <= 3) ? 'baby'
+        : (age > 3 && age <= 12) ? 'child'
+            : (age > 12 && age <= 19) ? 'teen'
+                : (age > 19 && age <= 64) ? 'adult'
+                    : (age > 64 && age <= 140) ? 'senior citizen'
+                        : 'This is not a valid age';
+};
+console.log(lifePhase(189));
